27010d2ac5a33e3bdfe64c3c40c8a9593fcdcc1a
xingliwei <362493656@qq.com>
2017-11-20 10:06:44 +0800

    完善注释

--- src/main/java/com/xlw/levyx/api/convert/DateConvert.java
+++ src/main/java/com/xlw/levyx/api/convert/DateConvert.java
@@ -8,6 +8,8 @@ import java.util.Date;
 
 /**
  * Created by levyx on 2017/7/17.
+ * 如请求参数中包含时间类型的数据，无法自动映射到Controller里的Date参数
+ * 需要使用@initBinder注解为binder提供一个数据的转换器，这个转换器可以自己实现，也可以用spring官方的一些实现。
  */
 public class DateConvert extends PropertyEditorSupport {
     private String pattern = "yyyy-MM-dd HH:mm:ss";
